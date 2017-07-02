f33a9dc12cea909c67005d940328f66df9542d81
Ary Daniel <eiga100593@gmail.com>
2017-07-01 22:18:05 -0500

    the POST was added to create an object

--- server.js
+++ server.js
@@ -45,6 +45,30 @@ router.get('/',callback)
 
 // more routes
 
+// on routes that end in /bears
+
+router.route('/bears')
+
+// create a bear (accesed at POST http://localhost:8080/api/bears)
+
+.post(function(req,res){
+
+	var bear= new Bear();     //create a new instance of the Bear
+
+	bear.name=req.body.name; // set the bears name (comes from the request)
+
+	// save the bear and check for errors
+
+	bear.save(function(err){
+
+		if (err)
+
+			res.send(err);
+		res.json({message: 'Bear has been created'});
+	});
+
+});
+
 //REGISTER OUR ROUTES ---------------------------------------------------------------------------
 
 // all of our routes will be prefixed with api
